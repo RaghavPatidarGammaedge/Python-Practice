f15c8ca8d2d29707922e482c23d18fa187dc4a80
RaghavPatidarGammaedge <raghav.patidar@gammaedge.io>
2025-07-22 16:13:41 +0530

    learned about loops and basic data structures in python

---
+++ loops.py
@@ -0,0 +1,27 @@
+
+#for loop
+for i in range(5):
+    print(i)
+
+
+#while loop
+i=10
+while i>0:
+    print(i)
+    i-=1
+
+
+#nested for loop
+for i in range(10):
+    for j in range(i):
+        print("*",end=' ')
+    print()
+
+#loops with data structures and else clause
+list1=[10,20,30,40,50]
+for i in list1:
+    if(i==33):
+        print(list1)
+        break
+else:
+    print(list1)
