915cdbe0ca0381e9389cae313bf1f7d62ddb703f
RaghavPatidarGammaedge <raghav.patidar@gammaedge.io>
2025-07-29 18:47:57 +0530

    daily updates 29/7/25

--- flaskDemo.py
+++ flaskDemo.py
@@ -1,30 +1,29 @@
 import sqlite3
 from flask import Flask,request
-import requests
+import psycopg2
 
+connection_info="host=localhost dbname=db1 user=rghv password=rghv"
+conn=psycopg2.connect(connection_info)
+cursor=conn.cursor()
 
-
-conn1=sqlite3.connect('flaskDB.db')
-cursor=conn1.cursor()
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS users (name TEXT PRIMARY KEY, password TEXT)")
-conn1.commit()
-conn1.close()
+cursor.execute("create table if not exists users (name text primary key, password text)")
+conn.commit()
+conn.close()
 
 app = Flask(__name__)
 
-@app.teardown_appcontext
+
 @app.route('/forPut',methods=['POST'])
 def forPut():
     name = request.form.get('name')
     password = request.form.get('password')
 
     try:
-        conn2 = sqlite3.connect('flaskDB.db')
+        conn2 = psycopg2.connect(connection_info)
         cursor = conn2.cursor()
-        cursor.execute('insert into users values (?,?)',(name,password))
-        x=cursor.execute("select * from users ")
-        print(x.fetchall())
+        cursor.execute("insert into users values ('"+name+"','"+password+"')")
+        cursor.execute("select * from users ")
+        print(cursor.fetchall())
         conn2.commit()
     except Exception as e:
         print(e)
@@ -41,12 +40,11 @@ def forPut():
 
 
 
-@app.teardown_appcontext
 @app.route('/forGet')
 def forGet():
 
     try:
-        conn3 = sqlite3.connect('flaskDB.db')
+        conn3 = psycopg2.connect(connection_info)
         cursor = conn3.cursor()
         cursor.execute('select * from users')
         conn3.commit()
@@ -60,5 +58,66 @@ def forGet():
     finally:
         conn3.close()
     return msg,n
+
+
+@app.route('/forDelete/<name>',methods=['DELETE'])
+def forDelete(name):
+    conn4 = psycopg2.connect(connection_info)
+
+    try:
+        cursor = conn4.cursor()
+        cursor.execute('delete from users where name=%s',(name))
+        conn4.commit()
+        msg='success'
+        n=200
+    except Exception as e:
+        print(e)
+        msg = 'failure'
+        n = 404
+    finally:
+        conn4.close()
+    return msg,n
+
+@app.route('/forUpdate/<name>',methods=['PUT'])
+def forUpdate(name):
+    conn5 = psycopg2.connect(connection_info)
+    password = request.form.get('password')
+    try:
+        cursor = conn5.cursor()
+        cursor.execute('update users set password=%s where name=%s',(password,name))
+        conn5.commit()
+        msg='success'
+        n=200
+    except Exception as e:
+        print(e)
+        msg = 'failure'
+        n = 404
+    finally:
+        conn5.close()
+
+    return msg,n
+
+@app.route('/forPatch/<name>',methods=['PATCH'])
+def forPatch(name):
+    conn6 = psycopg2.connect(connection_info)
+    password = request.form.get('password')
+    try:
+        cursor = conn6.cursor()
+        cursor.execute('update users set password=%s where name=%s',(password,name))
+        conn6.commit()
+        msg='success'
+        n=200
+    except Exception as e:
+        print(e)
+        msg = 'failure'
+        n = 404
+    finally:
+        conn6.close()
+
+    return msg,n
+
+
+
+
 
 if __name__ == '__main__': app.run()
---
+++ pandasDemo.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+li=[10,20,30,40,50]
+se=pd.Series(li)
+print(se)
+
+
+li2=[[10,20,30],[40,50,60],[70,80,90]]
+df=pd.DataFrame(li2)
+print(df)
